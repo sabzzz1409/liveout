ac831aa6380b7b7b95fc005fb419b02190f168e5
eyo cr <122462188+sabzzz1409@users.noreply.github.com>
2025-06-24 03:27:05 +0530

    Delete assets directory

--- assets/Api-CxRbXioc.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./main-j185796E.js";const s=()=>t.jsx(t.Fragment,{});export{s as default};
--- assets/Server_side_routing-CT-ZOznG.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./main-j185796E.js";const e=()=>r.jsx(r.Fragment,{});export{e as default};
